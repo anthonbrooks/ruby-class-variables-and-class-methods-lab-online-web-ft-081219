6121658b29fd68ba2144e667fa3968ae012d3931
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-23 02:45:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,7 +23,7 @@ class Song
     @@genres
   end
   
-  def genre_count
+  def self.genre_count
     @@genres.count
   end
   
@@ -31,7 +31,7 @@ class Song
     @@artists
   end
   
-  def artist_count
+  def self.artist_count
     @@artists.count  
   end
   
