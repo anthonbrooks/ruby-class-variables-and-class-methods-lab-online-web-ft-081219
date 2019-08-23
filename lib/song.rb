be6c1eaa8028141d1d680a3357cd74c81fee8d21
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-23 02:33:14 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song
+  
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
+  
+  def count
+    
+  end
+  
+  def genres 
+    
+  end
+  
+  def genre_count
+    
+  end
+  
+  def artist_count
+    
+  end
+  
+end
